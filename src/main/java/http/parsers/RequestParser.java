0df1487797356d2f434aefee81dc85d901beb9d3
Makis Otman <makisotman@gmail.com>
2015-02-01 18:58:37 +0000

    Add basic RequestParser

---
+++ src/main/java/http/parsers/RequestParser.java
@@ -0,0 +1,63 @@
+package http.parsers;
+
+import http.Exceptions.InvalidRequestMethodException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestParser {
+  public String getRequestLine(String request) {
+    String[] lines = separateRequestLines(request);
+    return lines[0];
+  }
+
+  public String requestMethod(String requestLine) {
+    String method = getLineContents(requestLine)[0];
+    if (invalidMethod(method)) {
+      throw new InvalidRequestMethodException();
+    }
+    return method;
+  }
+
+  public String requestURI(String requestLine) {
+    return getLineContents(requestLine)[1];
+  }
+
+  public Map<String, String> requestHeaders(String request) {
+    Map<String, String> headers = new HashMap<>();
+    addHeaderNameWithValue(headers, separateRequestLines(request));
+    return headers;
+  }
+
+  public String requestBody(String request) {
+    String[] lines = separateRequestLines(request);
+    return lines[lastLine(lines)];
+  }
+
+  private void addHeaderNameWithValue(Map<String, String> headers, String[] lines) {
+    for (int i = 1; i < lastLine(lines); i++) {
+      String[] header = lines[i].split(":\\s");
+      String headerName = header[0];
+      String headerValue = header[1];
+      headers.put(headerName, headerValue);
+    }
+  }
+
+  private String[] separateRequestLines(String request) {
+    return request.split("\r\n");
+  }
+
+  private String[] getLineContents(String requestLine) {
+    return requestLine.split("\\s");
+  }
+
+  private boolean invalidMethod(String method) {
+    String[] validMethods = {"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "CONNECT", "TRACE"};
+    return !Arrays.asList(validMethods).contains(method);
+  }
+
+  private int lastLine(String[] lines) {
+    return lines.length - 1;
+  }
+}
---
+++ src/test/java/http/parsers/RequestParserTest.java
@@ -0,0 +1,66 @@
+package http.parsers;
+
+import http.Exceptions.InvalidRequestMethodException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RequestParserTest {
+
+  private final String REQUEST_FIRST_LINE = "OPTIONS /some-uri HTTP/1.1\r\n";
+  private final String FULL_REQUEST = "GET / HTTP/1.1\r\n" +
+                                      "Accept-Language: en-us\r\n" +
+                                      "Host: localhost\r\n" +
+                                      "Body of the Request";
+  private RequestParser parser;
+
+  @Before
+  public void setup() {
+    parser = new RequestParser();
+  }
+
+  @Test
+  public void returnsTheRequestLine() {
+    String firstLine = "GET / HTTP/1.1";
+    String request = FULL_REQUEST;
+    assertThat(parser.getRequestLine(request), is(firstLine));
+  }
+
+  @Test
+  public void returnsTheMethod() {
+    String firstLine = REQUEST_FIRST_LINE;
+   assertThat(parser.requestMethod(firstLine), is("OPTIONS"));
+  }
+
+  @Test
+  public void returnsTheURI() {
+    String firstLine = REQUEST_FIRST_LINE;
+    assertThat(parser.requestURI(firstLine), is("/some-uri"));
+  }
+
+  @Test
+  public void returnsTheHeaders() {
+    String request = FULL_REQUEST;
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Accept-Language", "en-us");
+    headers.put("Host", "localhost");
+    assertThat(parser.requestHeaders(request), equalTo(headers));
+  }
+
+  @Test
+  public void returnsTheBodyOfTheRequest() {
+    assertThat(parser.requestBody(FULL_REQUEST), is("Body of the Request"));
+  }
+
+  @Test(expected = InvalidRequestMethodException.class)
+  public void rejectsUnknownRequest() {
+    String request = "INVALID-REQUEST / HTTP/1.1\r\n";
+    parser.requestMethod(request);
+  }
+}
