a638076601cde2e2de0aacaa5a4038683b6524cc
Makis Otman <makisotman@gmail.com>
2015-02-06 09:07:23 +0000

    Refactor BodyParser

--- src/main/java/http/parsers/BodyParser.java
+++ src/main/java/http/parsers/BodyParser.java
@@ -5,12 +5,13 @@ import java.io.BufferedReader;
 public class BodyParser {
   public String read(BufferedReader reader) {
     Object[] lines = reader.lines().toArray();
-    return getBodyLines(lines, findEmptyLine(lines));
+    return getBodyLines(lines);
   }
 
-  private String getBodyLines(Object[] lines, int emptyLineIndex) {
+  private String getBodyLines(Object[] lines) {
+    int emptyLineIndex = findEmptyLine(lines);
     String body = "";
-    for (int i = emptyLineIndex + 1; i < lines.length; i++) {
+    for (int i = emptyLineIndex; i < lines.length; i++) {
       body += lines[i] + "\n";
     }
     return body;
@@ -20,7 +21,7 @@ public class BodyParser {
     int emptyLineIndex = 0;
     for (int i = 0; i < lines.length; i++) {
       if(lines[i].equals("")) {
-        emptyLineIndex = i;
+        emptyLineIndex = i + 1;
       }
     }
     return emptyLineIndex;
