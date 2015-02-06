4893908290bd1d0bfd1c1d16ad8c4a80c94ed12c
Makis Otman <makisotman@gmail.com>
2015-02-06 09:24:59 +0000

    Refactor HeadersParser

--- src/main/java/http/parsers/HeadersParser.java
+++ src/main/java/http/parsers/HeadersParser.java
@@ -9,24 +9,32 @@ public class HeadersParser {
 
   private final int FIELD = 0;
   private final int VALUE = 1;
+  private Map<String, String> headers = new HashMap<>();
 
   public Map<String, String> read(BufferedReader reader) {
-    Map<String, String> headers = new HashMap<>();
     String line;
     try {
       while ((line = reader.readLine()) != null) {
-        if (line.equals("")) {
+        if (empty(line)) {
           break;
         }
-        if (line.contains(":")) {
-          String[] header = line.split(":", 2);
-          headers.put(clean(header[FIELD]), clean(header[VALUE]));
-        }
+        createHeaderFrom(line);
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
     return headers;
+  }
+
+  private boolean empty(String line) {
+    return line.equals("");
+  }
+
+  private void createHeaderFrom(String line) {
+    if (line.contains(":")) {
+      String[] header = line.split(":", 2);
+      headers.put(clean(header[FIELD]), clean(header[VALUE]));
+    }
   }
 
   private String clean(String s) {
