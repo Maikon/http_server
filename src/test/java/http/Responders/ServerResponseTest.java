ff5fa7f4defea573a10555c1e376cacf3f7e8b42
Makis Otman <makisotman@gmail.com>
2015-02-12 11:58:58 +0000

    Replace duplicated strings with constants in tests

--- src/test/java/http/Responders/ServerResponseTest.java
+++ src/test/java/http/Responders/ServerResponseTest.java
@@ -10,20 +10,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ServerResponseTest {
 
+  private final int OK = 200;
+  private final String VALUE_1 = "Value1";
+  private final String HEADER_2 = "Header2";
+  private final String VALUE_2 = "Value2";
+  private final String BODY = "Body";
+  private final String HEADER_1 = "Header1";
+  private final String STATUS_200_OK = "HTTP/1.1 200 OK\r\n";
+
   @Test
   public void buildsSuccessResponse() {
-    ServerResponse response = ServerResponse.status(200).build();
-    assertThat(response.statusLine(), is("HTTP/1.1 200 OK\r\n"));
+    ServerResponse response = ServerResponse.status(OK).build();
+    assertThat(response.statusLine(), is(STATUS_200_OK));
   }
 
   @Test
   public void addsMultipleHeadersToTheResponse() {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Header1", "Value1");
-    headers.put("Header2", "Value2");
-    ServerResponse response = ServerResponse.status(200)
-                                            .addHeader("Header1", "Value1")
-                                            .addHeader("Header2", "Value2")
+    headers.put(HEADER_1, VALUE_1);
+    headers.put(HEADER_2, VALUE_2);
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addHeader(HEADER_1, VALUE_1)
+                                            .addHeader(HEADER_2, VALUE_2)
                                             .build();
     assertThat(response.getHeaders(), is(headers));
   }
@@ -31,48 +39,48 @@ public class ServerResponseTest {
   @Test
   public void returnsHeadersInAStringFormat() {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Header1", "Value1");
-    headers.put("Header2", "Value2");
+    headers.put(HEADER_1, VALUE_1);
+    headers.put(HEADER_2, VALUE_2);
     String headersString = "Header1: Value1\r\nHeader2: Value2\r\n";
-    ServerResponse response = ServerResponse.status(200)
-                                            .addHeader("Header1", "Value1")
-                                            .addHeader("Header2", "Value2")
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addHeader(HEADER_1, VALUE_1)
+                                            .addHeader(HEADER_2, VALUE_2)
                                             .build();
     assertThat(response.stringifyHeaders(), is(headersString));
   }
 
   @Test
   public void addsBodyToTheRequest() {
-    ServerResponse response = ServerResponse.status(200)
-                                            .addBody("Body")
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addBody(BODY)
                                             .build();
-    assertThat(response.getBody(), is("Body"));
+    assertThat(response.getBody(), is(BODY));
   }
 
   @Test
   public void buildsFullResponse() {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Header1", "Value1");
-    ServerResponse response = ServerResponse.status(200)
-                                            .addHeader("Header1", "Value1")
-                                            .addBody("Body")
+    headers.put(HEADER_1, VALUE_1);
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addHeader(HEADER_1, VALUE_1)
+                                            .addBody(BODY)
                                             .build();
-    assertThat(response.statusLine(), is("HTTP/1.1 200 OK\r\n"));
+    assertThat(response.statusLine(), is(STATUS_200_OK));
     assertThat(response.getHeaders(), is(headers));
-    assertThat(response.getBody(), is("Body"));
+    assertThat(response.getBody(), is(BODY));
   }
 
   @Test
   public void returnsFullResponseInStringFormat() {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Header1", "Value1");
+    headers.put(HEADER_1, VALUE_1);
     String fullResponse = "HTTP/1.1 200 OK\r\n" +
                           "Header1: Value1\r\n" +
                           "\r\n" +
                           "Body";
-    ServerResponse response = ServerResponse.status(200)
-                                            .addHeader("Header1", "Value1")
-                                            .addBody("Body")
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addHeader(HEADER_1, VALUE_1)
+                                            .addBody(BODY)
                                             .build();
     assertThat(response.toString(), is(fullResponse));
   }
@@ -80,14 +88,14 @@ public class ServerResponseTest {
   @Test
   public void returnsFullResponseInBytes() {
     Map<String, String> headers = new HashMap<>();
-    headers.put("Header1", "Value1");
+    headers.put(HEADER_1, VALUE_1);
     byte[] fullResponse = ("HTTP/1.1 200 OK\r\n" +
                           "Header1: Value1\r\n" +
                           "\r\n" +
                           "Body").getBytes();
-    ServerResponse response = ServerResponse.status(200)
-                                            .addHeader("Header1", "Value1")
-                                            .addBody("Body")
+    ServerResponse response = ServerResponse.status(OK)
+                                            .addHeader(HEADER_1, VALUE_1)
+                                            .addBody(BODY)
                                             .build();
     assertThat(response.toBytes(), is(fullResponse));
   }
