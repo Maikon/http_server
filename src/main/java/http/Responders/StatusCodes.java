3b801e8edf5893b1c1582945ccf87b2e20de4fb7
Makis Otman <makisotman@gmail.com>
2015-02-15 23:26:52 +0000

    Add MethodNotAllowedController
    Responds with a 405.

--- src/main/java/http/Responders/StatusCodes.java
+++ src/main/java/http/Responders/StatusCodes.java
@@ -2,8 +2,9 @@ package http.responders;
 
 public enum StatusCodes {
   OK(200, "OK"),
+  REDIRECT(301, "Moved Permanently"),
   NOT_FOUND(404, "Not Found"),
-  REDIRECT(301, "Moved Permanently");
+  METHOD_NOT_ALLOWED(405, "Method Not Allowed");
 
   private final int code;
   private final String phrase;
