a56cdf54a55fba989653cffb9a1ead1809d789f5
Makis Otman <makisotman@gmail.com>
2015-02-21 21:59:29 +0000

    Create ParamsResponder

---
+++ src/main/java/http/Responders/ParamsResponder.java
@@ -0,0 +1,13 @@
+package http.responders;
+
+import http.Request;
+
+public class ParamsResponder implements Responder {
+
+  @Override
+  public ServerResponse response(Request request) {
+    return ServerResponse.status(StatusCodes.OK)
+                         .addBody(request.getParams())
+                         .build();
+  }
+}
---
+++ src/test/java/http/Responders/ParamsResponderTest.java
@@ -0,0 +1,26 @@
+package http.responders;
+
+import http.Request;
+import http.fakes.FakeClientSocket;
+import http.parsers.RequestParser;
+import org.junit.Test;
+
+import static http.responders.StatusCodes.OK;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ParamsResponderTest {
+
+  @Test
+  public void responseBodyContainsDecodedParams() {
+    String statusLine = "GET /params?%3Cexample-decoding%3E HTTP/1.1\r\n";
+    RequestParser parser = new RequestParser(new FakeClientSocket(statusLine));
+    Request request = parser.buildRequest();
+    Responder responder = new ParamsResponder();
+    ServerResponse response = responder.response(request);
+
+    assertThat(response.getStatus(), is(OK));
+    assertThat(response.getBody(), containsString("<example-decoding>"));
+  }
+}
