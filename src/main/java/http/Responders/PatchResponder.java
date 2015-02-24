764f83d837f60782140a8a0252b76591fe775bab
Makis Otman <makisotman@gmail.com>
2015-02-24 00:19:40 +0000

    Break up main conditional with lambda

--- src/main/java/http/Responders/PatchResponder.java
+++ src/main/java/http/Responders/PatchResponder.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static http.responders.StatusCodes.*;
 
@@ -21,23 +23,35 @@ public class PatchResponder implements Responder {
   @Override
   public ServerResponse response(Request request) {
     if (reader.fileExists(request)) {
-      String fileContents = reader.getFileContents(request);
-      if (request.getMethod().equals("GET")) {
-        return ServerResponse.status(OK)
-                             .addBody(fileContents)
-                             .build();
-      } else if (request.getMethod().equals("PATCH")) {
-        String fileSha = DigestUtils.sha1Hex(fileContents);
-        String requestSha = request.getHeaders().get("If-Match");
-        if (fileSha.equals(requestSha)) {
-          writeContentToFile(request, reader.findFile(request));
-          return ServerResponse.status(NO_CONTENT).build();
-        } else {
-          return ServerResponse.status(PRECONDITION_FAILED).build();
-        }
-      }
+      String method = request.getMethod();
+      Responder responder = responders().get(method);
+      return responder.response(request);
     }
     return ServerResponse.status(NOT_FOUND).build();
+  }
+
+  private Map<String, Responder> responders() {
+    Map<String, Responder> responders = new HashMap<>();
+    responders.put("GET", getResponder());
+    responders.put("PATCH", patchResponder());
+    return responders;
+  }
+
+  private Responder patchResponder() {
+    return request -> {
+      String fileSha = DigestUtils.sha1Hex(reader.getFileContents(request));
+      String requestSha = request.getHeaders().get("If-Match");
+      if (fileSha.equals(requestSha)) {
+        writeContentToFile(request, reader.findFile(request));
+        return ServerResponse.status(NO_CONTENT).build();
+      } else {
+        return ServerResponse.status(PRECONDITION_FAILED).build();
+      }
+    };
+  }
+
+  private Responder getResponder() {
+    return request -> ServerResponse.status(OK).addBody(reader.getFileContents(request)).build();
   }
 
   private void writeContentToFile(Request request, File file) {
