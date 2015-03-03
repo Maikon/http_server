b874c8b74fd8e29a88ec03053e1d784cee508731
Makis Otman <makisotman@gmail.com>
2015-03-03 17:34:49 +0000

    Extract registering of the routes into Main

--- src/main/java/http/Main.java
+++ src/main/java/http/Main.java
@@ -1,6 +1,9 @@
 package http;
 
+import http.filesystem.FileIO;
+import http.filters.Authenticator;
 import http.parsers.ArgumentsParser;
+import http.responders.*;
 import http.sockets.Socket;
 
 import java.io.File;
@@ -14,9 +17,11 @@ public class Main {
     int port = parser.integerValueFor("-p");
     String directory = parser.stringValueFor("-d");
     try {
-      Router router = new Router(new File(directory));
+      Router router = new Router();
       Socket clientSocket = new Socket(new ServerSocket(port));
       Worker worker = new Worker(router, clientSocket);
+      FileIO fileIO = new FileIO(new File(directory));
+      registerRoutes(router, fileIO);
       Server server = new Server(Executors.newFixedThreadPool(20), worker);
       System.out.println("Starting server at port: " + port);
       server.start();
@@ -24,5 +29,27 @@ public class Main {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private static void registerRoutes(Router router, FileIO fileIO) {
+    router.registerRoute("GET /", new RootResponder(fileIO));
+    router.registerRoute("GET /redirect", new RedirectResponder());
+    router.registerRoute("GET /foobar", new NotFoundResponder());
+    router.registerRoute("GET /file1", new FileContentsResponder(fileIO));
+    router.registerRoute("GET /form", new FormResponder(fileIO));
+    router.registerRoute("GET /patch-content.txt", new PatchResponder(fileIO));
+    router.registerRoute("GET /parameters", new ParamsResponder());
+    router.registerRoute("GET /logs", new BasicAuthResponder(new Authenticator("admin", "hunter2")));
+    router.registerRoute("GET /image.jpeg", new ImagesResponder(fileIO));
+    router.registerRoute("GET /image.png", new ImagesResponder(fileIO));
+    router.registerRoute("GET /image.gif", new ImagesResponder(fileIO));
+    router.registerRoute("GET /partial_content.txt", new RangeResponder(fileIO));
+    router.registerRoute("POST /form", new FormResponder(fileIO));
+    router.registerRoute("POST /text-file.txt", new MethodNotAllowedResponder());
+    router.registerRoute("PUT /form", new FormResponder(fileIO));
+    router.registerRoute("PUT /file1", new MethodNotAllowedResponder());
+    router.registerRoute("PATCH /patch-content.txt", new PatchResponder(fileIO));
+    router.registerRoute("DELETE /form", new FormResponder(fileIO));
+    router.registerRoute("OPTIONS /method_options", new MethodOptionsResponder());
   }
 }
--- src/main/java/http/Router.java
+++ src/main/java/http/Router.java
@@ -1,18 +1,15 @@
 package http;
 
-import http.filesystem.FileIO;
-import http.filters.Authenticator;
-import http.responders.*;
+import http.responders.NotFoundResponder;
+import http.responders.Responder;
 import http.utils.Logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Router {
-  private FileIO fileIO;
   private Map<String, Responder> responders = new HashMap<>();
   private String lastResponse;
 
@@ -20,18 +17,10 @@ public class Router {
     this.responders = generateResponders();
   }
 
-  public Router(File directory) {
-    this.fileIO = new FileIO(directory);
-    this.responders = generateResponders();
-  }
-
   public void dispatch(Request request, PrintStream output) throws IOException {
     String identifier = request.methodWithUri();
     Responder responder = responders.get(identifier);
     Logger.log(request);
-    if (request.getUri().contains("image")) {
-      responder = new ImagesResponder(fileIO);
-    }
     if (responder == null) {
       lastResponse = "404";
       responder = new NotFoundResponder();
@@ -52,22 +41,6 @@ public class Router {
   }
 
   private Map<String, Responder> generateResponders() {
-    registerRoute("GET /",                   new RootResponder(fileIO));
-    registerRoute("GET /redirect",           new RedirectResponder());
-    registerRoute("GET /foobar",             new NotFoundResponder());
-    registerRoute("GET /file1",              new FileContentsResponder(fileIO));
-    registerRoute("GET /form",               new FormResponder(fileIO));
-    registerRoute("POST /form",              new FormResponder(fileIO));
-    registerRoute("PUT /form",               new FormResponder(fileIO));
-    registerRoute("DELETE /form",            new FormResponder(fileIO));
-    registerRoute("POST /text-file.txt",     new MethodNotAllowedResponder());
-    registerRoute("PUT /file1",              new MethodNotAllowedResponder());
-    registerRoute("OPTIONS /method_options", new MethodOptionsResponder());
-    registerRoute("GET /parameters",         new ParamsResponder());
-    registerRoute("GET /patch-content.txt",  new PatchResponder(fileIO));
-    registerRoute("PATCH /patch-content.txt", new PatchResponder(fileIO));
-    registerRoute("GET /logs",               new BasicAuthResponder(new Authenticator("admin", "hunter2")));
-    registerRoute("GET /partial_content.txt", new RangeResponder(fileIO));
     return responders;
   }
 }
