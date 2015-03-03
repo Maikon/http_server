425444c864b2078aa1d6dfa773ac08391e36271f
Makis Otman <makisotman@gmail.com>
2015-03-03 17:57:14 +0000

    Improve Router tests
    - Verify correct behaviour of dispatch()
    - Remove duplication and dead code

--- src/main/java/http/Router.java
+++ src/main/java/http/Router.java
@@ -2,6 +2,7 @@ package http;
 
 import http.responders.NotFoundResponder;
 import http.responders.Responder;
+import http.responders.StatusCodes;
 import http.utils.Logger;
 
 import java.io.IOException;
@@ -11,24 +12,21 @@ import java.util.Map;
 
 public class Router {
   private Map<String, Responder> responders = new HashMap<>();
-  private String lastResponse;
-
-  public Router() {
-    this.responders = generateResponders();
-  }
+  private StatusCodes lastResponse;
 
   public void dispatch(Request request, PrintStream output) throws IOException {
     String identifier = request.methodWithUri();
     Responder responder = responders.get(identifier);
     Logger.log(request);
     if (responder == null) {
-      lastResponse = "404";
+      lastResponse = StatusCodes.NOT_FOUND;
       responder = new NotFoundResponder();
     }
+    lastResponse = responder.response(request).getStatus();
     output.write(responder.response(request).toBytes());
   }
 
-  public String lastResponse() {
+  public StatusCodes lastResponse() {
     return lastResponse;
   }
 
@@ -38,9 +36,5 @@ public class Router {
 
   public Responder getResponderFor(String identifier) {
     return responders.get(identifier);
-  }
-
-  private Map<String, Responder> generateResponders() {
-    return responders;
   }
 }
--- src/test/java/http/RouterTest.java
+++ src/test/java/http/RouterTest.java
@@ -2,6 +2,7 @@ package http;
 
 import http.responders.Responder;
 import http.responders.ServerResponse;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -10,56 +11,72 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import static http.responders.StatusCodes.NOT_FOUND;
+import static http.responders.StatusCodes.OK;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RouterTest {
+  private Router router;
+  private FakeOutput output;
+  private PrintStream printStream;
 
   @Rule
   public TemporaryFolder directory = new TemporaryFolder();
 
+  @Before
+  public void setUp() throws Exception {
+    router = new Router();
+    output = new FakeOutput();
+    printStream = new PrintStream(output);
+  }
+
   @Test
   public void respondsToKnownURI() throws IOException {
-    Router router = new Router(directory.getRoot());
-    FakeOutput output = new FakeOutput();
-    Request request = Request.withMethod("GET").addURI("/").build();
-    router.dispatch(request, new PrintStream(output));
-    assertThat(output.wasWritten, is(true));
+    router.registerRoute("GET /", new SuccessResponder());
+    Request request = buildRequest("GET", "/");
+    router.dispatch(request, printStream);
+    assertThat(output.wasWritten(), is(true));
+    assertThat(router.lastResponse(), is(OK));
   }
 
   @Test
   public void respondsToUnknownURI() throws IOException {
-    Router router = new Router();
-    Request request = Request.withMethod("GET").addURI("/unknown-path").build();
-    router.dispatch(request, new PrintStream(new FakeOutput()));
-    assertThat(router.lastResponse(), is("404"));
+    Request request = buildRequest("GET", "/unknown");
+    router.dispatch(request, printStream);
+    assertThat(router.lastResponse(), is(NOT_FOUND));
   }
 
   @Test
   public void registersARouteWithAResponder() {
     Router router = new Router();
-    router.registerRoute("GET /", new FakeResponder());
+    router.registerRoute("GET /", new SuccessResponder());
     assertThat(router.getResponderFor("GET /"), isA(Responder.class));
   }
 
-  private class FakeOutput extends OutputStream {
-    private boolean wasWritten;
+  private Request buildRequest(String method, String uri) {
+    return Request.withMethod(method).addURI(uri).build();
+  }
 
-    public FakeOutput() {
-      this.wasWritten = false;
-    }
+  private class FakeOutput extends OutputStream {
+    private boolean written = false;
 
     @Override
     public void write(int response) throws IOException {
-      wasWritten = true;
+      written = true;
+    }
+
+    public boolean wasWritten() {
+      return written;
     }
   }
 
-  private class FakeResponder implements Responder {
+  private class SuccessResponder implements Responder {
+
     @Override
     public ServerResponse response(Request request) {
-      return null;
+      return ServerResponse.status(OK).build();
     }
   }
 }
