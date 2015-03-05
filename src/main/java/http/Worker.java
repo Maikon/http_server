c4628ce693d2b7dca2123fc51d36b2e8509a3599
Makis Otman <makisotman@gmail.com>
2015-03-05 16:24:46 +0000

    Add Logger in Worker

--- src/main/java/http/Worker.java
+++ src/main/java/http/Worker.java
@@ -2,11 +2,13 @@ package http;
 
 import http.parsers.RequestParser;
 import http.sockets.ClientSocket;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.PrintStream;
 
 public class Worker implements Runnable {
+  private final static Logger logger = Logger.getLogger(Worker.class);
   private final Router router;
   private final RequestParser reqParser;
   private final ClientSocket client;
@@ -23,7 +25,7 @@ public class Worker implements Runnable {
     try {
       router.dispatch(reqParser.buildRequest(), output);
     } catch (IOException e) {
-      //
+      logger.error("Exception Occurred: " + e.getMessage());
     }
     client.close();
   }
