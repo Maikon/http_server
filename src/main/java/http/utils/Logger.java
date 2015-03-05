6b5e5f2056e89087f7d05a541a2af704ab67b164
Makis Otman <makisotman@gmail.com>
2015-03-05 19:02:21 +0000

    Add small wrapper for Log4j

---
+++ src/main/java/http/utils/Logger.java
@@ -0,0 +1,18 @@
+package http.utils;
+
+public class Logger {
+
+  private final org.apache.log4j.Logger logger;
+
+  public Logger(org.apache.log4j.Logger logger) {
+    this.logger = logger;
+  }
+
+  public void logError(Exception message) {
+    logger.error(message);
+  }
+
+  public void logInfo(String message) {
+    logger.info(message);
+  }
+}
---
+++ src/test/java/http/utils/LoggerTest.java
@@ -0,0 +1,36 @@
+package http.utils;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+public class LoggerTest {
+
+  private org.apache.log4j.Logger loggerSpy;
+  private Logger loggerWrapper;
+
+  @Before
+  public void setUp() {
+    BasicConfigurator.configure();
+    org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger("FakeClass");
+    loggerSpy = spy(logger);
+    loggerWrapper = new Logger(loggerSpy);
+  }
+
+  @Test
+  public void logsExceptionMessages() {
+    Exception exception = new Exception();
+    loggerWrapper.logError(exception);
+    verify(loggerSpy).error(exception);
+  }
+
+  @Test
+  public void logsMessagesOnStdOut() {
+    String message = "Some Information";
+    loggerWrapper.logInfo(message);
+    verify(loggerSpy).info(message);
+  }
+}
