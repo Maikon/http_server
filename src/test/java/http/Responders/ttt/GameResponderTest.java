d2246ec1b59a03125f235e46756fa81e50e9db47
Makis Otman <makisotman@gmail.com>
2015-03-12 19:38:50 +0000

    Change index template to include board sizes

--- src/test/java/http/Responders/ttt/GameResponderTest.java
+++ src/test/java/http/Responders/ttt/GameResponderTest.java
@@ -7,18 +7,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static http.responders.StatusCodes.OK;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.AllOf.allOf;
 
 public class GameResponderTest {
     private GameResponder responder;
     private Request request;
+    private RetrieveTemplate template;
 
     @Before
     public void setUp() {
-        responder = new GameResponder(new RetrieveTemplate("/index.html"));
+        template = new RetrieveTemplate("/index.html");
+        responder = new GameResponder(template);
         request = buildRequest("GET", "/game");
     }
 
@@ -29,12 +29,9 @@ public class GameResponderTest {
     }
 
     @Test
-    public void responseContainsGameOptions() {
+    public void responseContainsTemplateContents() {
         ServerResponse response = responder.response(request);
-        assertThat(response.getBody(), allOf(containsString("Human vs Human"),
-                                             containsString("Human vs Computer"),
-                                             containsString("Computer vs Human"),
-                                             containsString("Computer vs Computer")));
+        assertThat(response.getBody().getBytes(), is(template.inBytes()));
     }
 
     @Test
@@ -42,6 +39,7 @@ public class GameResponderTest {
         ServerResponse response = responder.response(request);
         assertThat(response.getHeader("Content-Type"), is("text/html"));
     }
+
 
     private Request buildRequest(String method, String uri) {
         return Request.withMethod(method)
